3d6d7565ea764707fe5c4f9f07bf158dad2534de
kabilan086 <kabilanr086@gmail.com>
2025-02-13 00:41:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value.trim();
+
+    if (taskText === "") return;
+
+    let li = document.createElement("li");
+    li.innerHTML = `${taskText} <button onclick="removeTask(this)">Delete</button>`;
+
+    document.getElementById("taskList").appendChild(li);
+    taskInput.value = ""; 
+}
+
+function removeTask(button) {
+    button.parentElement.remove();
+}
+ 
